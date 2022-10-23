1dfd7d5b4e9f747ca6000cdaa21bbb7ac3e63633
izzatmuzaki <izzat.muzaki14@gmail.com>
2022-10-24 00:11:15 +0700

    FIX: Tables

---
+++ src/views/Tables.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="py-4 container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <ProjectsTable />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ProjectsTable from "./components/ProjectsTable.vue";
+</script>
